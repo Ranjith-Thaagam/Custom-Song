ada1026b3c601d613977406d424d2445fae189ae
PremRajzC <premraj@thaagam.email>
2025-11-05 14:19:42 +0530

    add structure.py

---
+++ structure.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+dir = 'src'
+files_name=[
+    f'{dir}/models/encoder.py',
+    f'{dir}/models/decoder.py',
+    f'{dir}/laguage/LangSegment.py',
+]
+
+for files in files_name:
+    path_file = Path(files)
+
+    floder ,file  = os.path.split(path_file)
+    
+    if not os.path.exists(floder):
+        os.makedirs(floder)
+
+    if not os.path.exists(path_file):
+        with open(path_file, 'w') as f:
+            pass
+    
